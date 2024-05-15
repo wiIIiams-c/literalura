04e3265112cfda2d1c2452f7d3a403e1d9682d23
Williams C <wilcontrerasf@gmail.com>
2024-05-14 20:56:06 -0400

    Create Model API
    Se crean los modelos que interacturan con la api y mapearan los campos necesarios

---
+++ src/main/java/com/alurachallenge/literalura/model/DatosLibro.java
@@ -0,0 +1,16 @@
+package com.alurachallenge.literalura.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibro(
+    @JsonAlias("title") String titulo,
+    @JsonAlias("authors") List<DatosAutor> autor,
+    @JsonAlias("languages") List<String> idioma,
+    @JsonAlias("download_count") Long numeroDescargas
+) {
+
+}
