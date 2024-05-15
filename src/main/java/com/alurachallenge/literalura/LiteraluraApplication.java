9e6cbe8f182c26b56e26c87472d64ca9f0339829
Williams C <wilcontrerasf@gmail.com>
2024-05-14 20:54:44 -0400

    Update LiteraluraApplication
    se implementa el CommanLineRunner para ejecutar en consola
    
    se llama el metodo run para mostrar el menu y ejecutar el programa

--- src/main/java/com/alurachallenge/literalura/LiteraluraApplication.java
+++ src/main/java/com/alurachallenge/literalura/LiteraluraApplication.java
@@ -1,13 +1,22 @@
 package com.alurachallenge.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alurachallenge.literalura.principal.Principal;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.iniciar();
 	}
 
 }
