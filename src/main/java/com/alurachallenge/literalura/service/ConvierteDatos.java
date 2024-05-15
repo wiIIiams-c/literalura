7bf6aeb36805acd59e20f56c772e885438a33b23
Williams C <wilcontrerasf@gmail.com>
2024-05-14 20:59:53 -0400

    Creacion servicios
    se crean los servicios que van a interactuar con la API

---
+++ src/main/java/com/alurachallenge/literalura/service/ConvierteDatos.java
@@ -0,0 +1,18 @@
+package com.alurachallenge.literalura.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConvierteDatos implements IConvierteDatos{
+    private ObjectMapper om = new ObjectMapper();
+
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return om.readValue(json, clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
