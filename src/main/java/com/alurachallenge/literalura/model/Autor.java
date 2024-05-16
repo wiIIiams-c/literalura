af4e082a29f8d3737e9c87a81bb9ff85b2de529e
Williams C <wilcontrerasf@gmail.com>
2024-05-15 21:54:09 -0400

    Create clases db
    se crean las clases que seran las entidades de la db

--- src/main/java/com/alurachallenge/literalura/model/Autor.java
+++ src/main/java/com/alurachallenge/literalura/model/Autor.java
@@ -1,5 +1,70 @@
 package com.alurachallenge.literalura.model;
 
-public class Autor {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAutor;
+    private String nombre;
+    private String fechaNacimiento;
+    private String fechaFallecimiento;
+
+    @OneToOne
+    private Libro libro;
+
+    public Autor(DatosAutor datosAutor){
+        this.nombre = datosAutor.nombre();
+        this.fechaNacimiento = datosAutor.fechaNacimiento();
+        this.fechaFallecimiento = datosAutor.fechaFallecimiento();
+    }
+
+    public Long getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(Long idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getFechaFallecimiento() {
+        return fechaFallecimiento;
+    }
+
+    public void setFechaFallecimiento(String fechaFallecimiento) {
+        this.fechaFallecimiento = fechaFallecimiento;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    
 }
--- src/main/java/com/alurachallenge/literalura/model/Libro.java
+++ src/main/java/com/alurachallenge/literalura/model/Libro.java
@@ -1,9 +1,71 @@
 package com.alurachallenge.literalura.model;
 
-import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "libros")
 public class Libro {
+    @Id
+    private Long idLibro;
+
+    @Column(unique = true)
     private String titulo;
-    private List<Autor> autor;
-    private List<String> idioma;
+
+    @OneToOne(mappedBy = "libro", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Autor autor;
+    private String idioma;
+    private Long numeroDescargas;
+
+    public Libro(DatosLibro datosLibro){
+        this.idLibro = datosLibro.idLibro();
+        this.titulo = datosLibro.titulo();
+        this.idioma = datosLibro.idioma().get(0);
+        this.numeroDescargas = datosLibro.numeroDescargas();
+    }
+
+    public Long getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(Long idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Long getNumeroDescargas() {
+        return numeroDescargas;
+    }
+
+    public void setNumeroDescargas(Long numeroDescargas) {
+        this.numeroDescargas = numeroDescargas;
+    }
 }
