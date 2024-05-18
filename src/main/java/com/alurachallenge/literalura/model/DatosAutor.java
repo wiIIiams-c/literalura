0b6f38c9e3e93e19a30e689767c8424b3288a1bb
Williams C <wilcontreras.developer@gmail.com>
2024-05-17 22:00:31 -0400

    Update DatosAutor
    se cambie el nombre fecha por anio

--- src/main/java/com/alurachallenge/literalura/model/DatosAutor.java
+++ src/main/java/com/alurachallenge/literalura/model/DatosAutor.java
@@ -6,8 +6,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosAutor(
     @JsonAlias("name") String nombre,
-    @JsonAlias("birth_year") String fechaNacimiento,
-    @JsonAlias("death_year") String fechaFallecimiento
+    @JsonAlias("birth_year") String anioNacimiento,
+    @JsonAlias("death_year") String anioFallecimiento
 ) {
 
 }
