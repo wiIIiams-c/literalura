50b4d0ff6903dc4d08257db19777c739824079e9
Williams C <wilcontreras.developer@gmail.com>
2024-05-17 21:59:45 -0400

    Update LiteraluraApplication
    se agrega el autowired para el repository

--- src/main/java/com/alurachallenge/literalura/LiteraluraApplication.java
+++ src/main/java/com/alurachallenge/literalura/LiteraluraApplication.java
@@ -1,13 +1,17 @@
 package com.alurachallenge.literalura;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alurachallenge.literalura.principal.Principal;
+import com.alurachallenge.literalura.repository.LibroRespository;
 
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner{
+	@Autowired
+	private LibroRespository respository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
@@ -15,7 +19,7 @@ public class LiteraluraApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(respository);
 		principal.iniciar();
 	}
 
