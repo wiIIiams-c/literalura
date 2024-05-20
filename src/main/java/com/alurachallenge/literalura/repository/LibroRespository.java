16aee68c888d86107ea5c4623279bfed27c26d07
Williams C <wilcontreras.developer@gmail.com>
2024-05-19 21:57:14 -0400

    Delete
    Error de nombre en LibroRespository

--- src/main/java/com/alurachallenge/literalura/repository/LibroRespository.java
+++
@@ -1,9 +0,0 @@
-package com.alurachallenge.literalura.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alurachallenge.literalura.model.Libro;
-
-public interface LibroRespository extends JpaRepository<Libro, Long>{
-
-}
