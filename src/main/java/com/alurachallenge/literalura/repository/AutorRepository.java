7b27a7c0028f4e38c2cbbd71f0ff974a14d2939f
Williams C <wilcontreras.developer@gmail.com>
2024-05-20 21:34:46 -0400

    Update AutorRepository
    consulta agregada para traer los autores vivos para el año consultado, retorna una lista de autores

--- src/main/java/com/alurachallenge/literalura/repository/AutorRepository.java
+++ src/main/java/com/alurachallenge/literalura/repository/AutorRepository.java
@@ -1,11 +1,16 @@
 package com.alurachallenge.literalura.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.alurachallenge.literalura.model.Autor;
 
 public interface AutorRepository extends JpaRepository<Autor, Long>{
     Optional<Autor> findByNombre(String nombre);
+
+    @Query("SELECT a FROM Autor a WHERE a.anioNacimiento < :anio AND a.anioFallecimiento > :anio")
+    List<Autor> obtenerAutorVivoAnio(int anio);
 }
