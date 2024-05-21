c0608d2dac39aeb418666c5ed4197471bf59fc44
Williams C <wilcontreras.developer@gmail.com>
2024-05-20 21:23:46 -0400

    Update DatosAutor
    Se cambian los de años de string a integer

--- src/main/java/com/alurachallenge/literalura/model/DatosAutor.java
+++ src/main/java/com/alurachallenge/literalura/model/DatosAutor.java
@@ -6,8 +6,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosAutor(
     @JsonAlias("name") String nombre,
-    @JsonAlias("birth_year") String anioNacimiento,
-    @JsonAlias("death_year") String anioFallecimiento
+    @JsonAlias("birth_year") Integer anioNacimiento,
+    @JsonAlias("death_year") Integer anioFallecimiento
 ) {
 
 }
