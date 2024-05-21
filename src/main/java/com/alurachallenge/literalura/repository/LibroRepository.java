0396e8c8cbb944b8436ce64723c927492ee561dc
Williams C <wilcontreras.developer@gmail.com>
2024-05-20 21:35:47 -0400

    Update LibroRepository
    se agrega busqueda de libro por Id de este

--- src/main/java/com/alurachallenge/literalura/repository/LibroRepository.java
+++ src/main/java/com/alurachallenge/literalura/repository/LibroRepository.java
@@ -1,9 +1,11 @@
 package com.alurachallenge.literalura.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alurachallenge.literalura.model.Libro;
 
 public interface LibroRepository extends JpaRepository<Libro, Long>{
-
+    Optional<Libro> findByIdLibro(Long idLibro);
 }
