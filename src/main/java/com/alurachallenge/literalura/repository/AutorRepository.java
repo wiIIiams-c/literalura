0f07ac6544da6c49e0037dc42e8e4378142f4d98
Williams C <wilcontreras.developer@gmail.com>
2024-05-21 21:57:34 -0400

    Update AutorRespository
    Se agrega a la consulta de autor por año el order by nombre autor ascendente

--- src/main/java/com/alurachallenge/literalura/repository/AutorRepository.java
+++ src/main/java/com/alurachallenge/literalura/repository/AutorRepository.java
@@ -9,8 +9,9 @@ import org.springframework.data.jpa.repository.Query;
 import com.alurachallenge.literalura.model.Autor;
 
 public interface AutorRepository extends JpaRepository<Autor, Long>{
+    List<Autor> findAllByOrderByNombreAsc();
     Optional<Autor> findByNombre(String nombre);
 
-    @Query("SELECT a FROM Autor a WHERE a.anioNacimiento < :anio AND a.anioFallecimiento > :anio")
+    @Query("SELECT a FROM Autor a WHERE a.anioNacimiento < :anio AND a.anioFallecimiento > :anio ORDER BY a.nombre ASC")
     List<Autor> obtenerAutorVivoAnio(int anio);
 }
