6a2eb8da54c307e3791a486aa03ac2b665c5ceed
Williams C <wilcontreras.developer@gmail.com>
2024-05-21 21:57:00 -0400

    Update LibroRespository
    Se agrego consulta que trae los libros de un determinado autor
    
    Se deja consulta que trae los idiomas que se encuentran en la db de los libros

--- src/main/java/com/alurachallenge/literalura/repository/LibroRepository.java
+++ src/main/java/com/alurachallenge/literalura/repository/LibroRepository.java
@@ -1,11 +1,19 @@
 package com.alurachallenge.literalura.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.alurachallenge.literalura.model.Libro;
 
 public interface LibroRepository extends JpaRepository<Libro, Long>{
     Optional<Libro> findByIdLibro(Long idLibro);
+
+    @Query("SELECT DISTINCT l.idioma from Libro l ORDER BY l.idioma")
+    List<String> obtenerListaUnicaIdioma();
+
+    @Query("SELECT l FROM Autor a JOIN a.libros l WHERE a.idAutor = :id")
+    List<Libro> obtenerLibrosPorAutor(Long id);
 }
